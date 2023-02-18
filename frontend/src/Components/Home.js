c3380c29e199a985cdb1a5ce0a33f5eb98f53aa0
IDY12345 <106066822+IDY12345@users.noreply.github.com>
2023-02-18 13:56:19 +0530

    Change in Home Page CSS

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -3,13 +3,12 @@ import './Home.css'
 import Shop from './Shop'
 function Home() {
   return (
-    <div>
-        <div className="Hero">
+    <div >
+        <div class="Home-Page">
             <div class="card text-bg-dark border-0">
-                <img src="/assets/bg2.jpg" class="card-img" alt="Background" height="1035px" width="100%" />
                 <div class="card-img-overlay">
-                    <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-                    <p class="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+                    <h1 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h1>
+                    <h3 class="card-text lead fs-2" color='white'>CHECK OUT ALL THE TRENDS</h3>
                     
                 </div>
             </div>
